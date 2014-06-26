383b85518c76d1dd2f9242ee328fa0fc2f5d4814
Vladislav Kaznacheev <kaznacheev@chromium.org>
2014-06-27 01:05:56 +0400

    Moved all the code into the background page.

--- proxy_app/js/background.js
+++ proxy_app/js/background.js
@@ -1,14 +1,120 @@
-/**
- * Listens for the app launching then creates the window
- *
- * @see http://developer.chrome.com/apps/app.window.html
- */
+var RUN_IN_BACKGROUND_KEY = "runInBackground";
+var RUN_PROXY_KEY = "runProxyServer";
+var RUN_DEVICE_KEY = "runTestDevice";
+var CONNECT_LOCALHOST_KEY = "connectToLocalhost";
+
+function storeSetting(key, on) {
+  if (!!window[key] == !!on)
+    return false;
+
+  window[key] = !!on;
+  if (on) {
+    var items = {};
+    items[key] = true;
+    chrome.storage.local.set(items);
+  } else {
+    chrome.storage.local.remove(key);
+  }
+  return true;
+}
+
+function changeSetting(key, on) {
+  if (!storeSetting(key, on))
+    return;
+
+  switch (key) {
+    case RUN_PROXY_KEY:
+      if (on)
+        createProxyServer();
+      else
+        deleteProxyServer();
+      break;
+
+    case RUN_DEVICE_KEY:
+      if (on)
+        TestDevice.start();
+      else
+        TestDevice.stop();
+      break;
+
+    case CONNECT_LOCALHOST_KEY:
+      if (window[RUN_PROXY_KEY]) {
+        deleteProxyServer();
+        createProxyServer();
+      }
+      break;
+  }
+}
+
+var server;
+var connector;
+
+function createProxyServer() {
+  if (window[CONNECT_LOCALHOST_KEY])
+    connector = new TestDeviceConnector();
+  else
+    connector = new DeviceConnector();
+
+  var LOCAL_HOST = "127.0.0.1";
+  var ADB_PORT = 5037;
+  server = new TCP.Server(LOCAL_HOST, ADB_PORT, AdbCommandHandler, connector);
+}
+
+function deleteProxyServer() {
+  server.close();
+  server = null;
+  connector.stop();
+  connector = null;
+}
+
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('main.html', {
-  	id: "mainwin",
-    bounds: {
-      width: 500,
-      height: 640
-    }
-  });
+  chrome.app.window.create('main.html',
+      {
+        id: "settings",
+        bounds: {
+          width: 600,
+          height: 300
+        }
+      },
+      onWindowOpen);
+});
+
+function onWindowOpen(win) {
+  win.onClosed.addListener(onWindowClosed);
+
+  if (window[RUN_IN_BACKGROUND_KEY])
+    return;
+
+  if (window[RUN_PROXY_KEY])
+    createProxyServer();
+
+  if (window[RUN_DEVICE_KEY])
+    TestDevice.start();
+}
+
+function onWindowClosed() {
+  if (window[RUN_IN_BACKGROUND_KEY])
+    return;
+
+  if (window[RUN_PROXY_KEY])
+    deleteProxyServer();
+
+  if (window[RUN_DEVICE_KEY])
+    TestDevice.stop();
+}
+
+chrome.storage.local.get(function(items) {
+  window[RUN_IN_BACKGROUND_KEY] = items[RUN_IN_BACKGROUND_KEY];
+  window[RUN_PROXY_KEY] = items[RUN_PROXY_KEY];
+  window[RUN_DEVICE_KEY] = items[RUN_DEVICE_KEY];
+  window[CONNECT_LOCALHOST_KEY] = items[CONNECT_LOCALHOST_KEY];
+
+  if (!window[RUN_IN_BACKGROUND_KEY])
+    return;
+
+  if (window[RUN_PROXY_KEY])
+    createProxyServer();
+
+  if (window[RUN_DEVICE_KEY])
+    TestDevice.start();
 });
--- proxy_app/js/main.js
+++ proxy_app/js/main.js
@@ -1,41 +1,25 @@
 onload = function() {
-  var connector;
-  var proxyServer;
+  function setupSettingCheckbox(key) {
+    var checkbox = document.getElementById(key);
 
-  function createProxyServer(connector) {
-    var LOCAL_HOST = "127.0.0.1";
-    var ADB_PORT = 5037;
-    return new TCP.Server(LOCAL_HOST, ADB_PORT, AdbCommandHandler, connector);
+    chrome.runtime.getBackgroundPage(function(background) {
+      if (background[key])
+        checkbox.checked = true;
+    });
+
+    checkbox.addEventListener('click', function(event) {
+      chrome.runtime.getBackgroundPage(function(background) {
+        try {
+          background.changeSetting(key, event.target.checked);
+        } catch (e) {
+          console.error(e.stack);
+        }
+      });
+    });
   }
 
-  function setClickHandler(id, handler) {
-    document.getElementById(id).addEventListener('click', handler);
-  }
-
-  setClickHandler('client-start', function() {
-    if (connector)
-      return;
-    connector = new DeviceConnector();
-    proxyServer = createProxyServer(connector);
-  });
-
-  setClickHandler('client-start-loopback', function() {
-    if (connector)
-      return;
-    connector = new TestDeviceConnector();
-    proxyServer = createProxyServer(connector);
-  });
-
-  setClickHandler('client-stop', function() {
-    if (!connector)
-      return;
-    connector.stop();
-    connector = null;
-    proxyServer.close();
-    proxyServer = null;
-  });
-
-  setClickHandler('server-start', TestDevice.start);
-
-  setClickHandler('server-stop', TestDevice.stop);
+  setupSettingCheckbox('runInBackground');
+  setupSettingCheckbox('runProxyServer');
+  setupSettingCheckbox('runTestDevice');
+  setupSettingCheckbox('connectToLocalhost');
 };
