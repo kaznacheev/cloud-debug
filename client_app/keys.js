4bcc7d1754825160df3d0c1c4bfd20909ca514ed
Vladislav Kaznacheev <kaznacheev@chromium.org>
2014-06-30 17:32:09 +0400

    Generated new keys for the client app.

--- client_app/keys.js
+++ client_app/keys.js
@@ -1,4 +1,6 @@
+// id: klpnemgaegfjklfmlekmmnobcefcpapd
+
 var Keys = {
   API_KEY: "AIzaSyCpN9Dle8T9vD86D0KutoO9sGBk6_66dcw",
-  OAUTH_CLIENT_ID: "852539952535-bijes4q2bihgbv1u66clto2qgedsp4ef.apps.googleusercontent.com"
+  OAUTH_CLIENT_ID: "852539952535-dbmvt973p9ql846hcpeebaduvnvqbv93.apps.googleusercontent.com"
 };
