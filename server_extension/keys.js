18919f46af528f1fcd3556803371ae8086de61cb
Vladislav Kaznacheev <kaznacheev@chromium.org>
2014-06-30 17:22:40 +0400

    Server manifest changes

--- server_extension/keys.js
+++ server_extension/keys.js
@@ -1,3 +1,5 @@
+// id: lmbbfoodmokjhbnhbcihdkegdhgigjfb
+
 var Keys = {
   API_KEY: "AIzaSyCpN9Dle8T9vD86D0KutoO9sGBk6_66dcw",
   OAUTH_CLIENT_ID: "852539952535-psnr46jkj7ejfgn77rj6ntscfrkvblrs.apps.googleusercontent.com"
